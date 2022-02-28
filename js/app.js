004648abf7f61bda802d4a83deccd79fef56e127
devnoman01 <devnoman01@gmail.com>
2022-03-01 05:06:41 +0600

    Onclick fetch & show item details complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 const allPhoneUrl = `https://openapi.programming-hero.com/api/phones?search=`;
 
 
-const showDetails = (id) => {
+const displayDetails = (info) => {
+    const parentDiv = document.getElementById('details-div');
+    parentDiv.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add("row", "border", "rounded", "py-3");
+    div.innerHTML = `
+        <div class="col-md-4 col-sm-12 p-4 text-center">
+            <img class="w-75" src="${info.image}" alt="">
+        </div>
+        <div class="col-md-8 col-sm-12">
+            <h4>${info.name}</h4>
+            <small>${info.releaseDate}</small>
+            <p class="fw-bold my-2">Main Feature:</p>
+            <li><small>${info.mainFeatures.chipSet}</small></li>
+            <li><small>${info.mainFeatures.displaySize}</small></li>
+            <li><small>${info.mainFeatures.memory}</small></li>
+            <li><small>${info.mainFeatures.storage}</small></li>
+        </div>
+    `;
+    parentDiv.appendChild(div);
+    document.getElementById('pop-up-section').style.display = "block";
+}
+
+const detailsInfo = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data.data));
+    .then(data => displayDetails(data.data));
 }
 
 
@@ -24,7 +47,7 @@ const displayPhones = (phones) => {
                         <h4 class="card-title">${phone.phone_name}</h4>
                         <span class="mb-2">Brand: ${phone.brand}</span>
                         <br>
-                        <button onclick="showDetails('${phone.slug}')" data-toggle="modal" data-target="#myModal" class="mt-3 btn button">Details</button>                        
+                        <button onclick="detailsInfo('${phone.slug}')" data-toggle="modal" data-target="#myModal" class="mt-3 btn button">Details</button>                        
                     </div>
                 </div>
             `;
