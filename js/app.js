ac99afccf350cf3d41bd92abbdd6dbd35bdb0ca1
devnoman01 <devnoman01@gmail.com>
2022-03-01 01:39:51 +0600

    20 item fetch & load complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('Connection successful');
+const allPhoneUrl = `https://openapi.programming-hero.com/api/phones?search=`;
+
+
+const displayPhones = (phones) => {
+    let phoneCount = 0;
+    phones.forEach(phone => {
+        if(phoneCount < 20){
+            console.log(phone);
+            phoneCount++;
+        }
+        else{
+            return;
+        }
+    });
+}
+
+
+const loadPhones = (url) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayPhones(data.data));
+}
+
+
+onload = loadPhones(allPhoneUrl);
